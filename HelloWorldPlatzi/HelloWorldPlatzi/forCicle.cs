03e6a578d6f8d713d5a14171ca99b1a2c473a92e
Franco Alva <franco99alva@gmail.com>
2022-02-07 17:04:18 -0500

    se hace un commit hasta la clase 22

---
+++ HelloWorldPlatzi/HelloWorldPlatzi/forCicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldPlatzi
+{
+    internal class forCicle
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 50; i = i + 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
